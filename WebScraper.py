4f822434092a86743b7c4af02dd88831c8761114
Omar Gonzalez <omargb22@gmail.com>
2018-10-18 18:51:42 -0700

    first commit starting

---
+++ WebScraper.py
@@ -0,0 +1,13 @@
+"""
+Omat Gonzalez 
+web scraper python project 
+"""
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+	def __init__(self,site):
+		self.site=site
+
+	def scrape(self):
+		pass
